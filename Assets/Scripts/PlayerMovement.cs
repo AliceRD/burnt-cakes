8eaf4192918df0e153e8fb67caff877ff4d82efd
alicerd <alicerobertsdunn@gmail.com>
2018-04-04 23:25:38 +0100

    Add animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+public float speed = 3.0F;
+public float drag = 1;
+		
+//public float rotationSpeed = 100.0F;
+void Update() {
+	float translation = Input.GetAxisRaw("Vertical") * speed;
+	float translation2 = Input.GetAxisRaw("Horizontal") * speed;
+	translation *= Time.deltaTime;
+	translation2 *= Time.deltaTime;
+		transform.Translate(translation2, translation, 0);
+	//transform.Rotate(0, rotation, 0);
+}
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour 
+{
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+		animator.SetInteger ("Direction", 5);
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		var vertical = Input.GetAxisRaw ("Vertical"); 
+		var horizontal = Input.GetAxisRaw ("Horizontal");
+
+		if (vertical > 0) {
+			animator.SetInteger ("Direction", 2);
+		} else if (vertical < 0) {
+			animator.SetInteger ("Direction", 0);
+		} else if (horizontal > 0) {
+			animator.SetInteger ("Direction", 3);
+		} else if (horizontal < 0) {
+			animator.SetInteger ("Direction", 1);
+		} else if (vertical == 0) {
+			animator.SetInteger ("Direction", 5);
+		} else if (horizontal == 0) {
+			animator.SetInteger ("Direction", 5);
+		}
+		int val = animator.GetInteger ("Direction");
+		Debug.Log (val);
+	}
+}
+	
